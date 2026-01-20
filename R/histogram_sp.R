2e375001751e855955f249d96a80a182cecf6d1c
snhof <s.n.hof@amsterdamumc.nl>
2026-01-20 13:59:57 +0100

    fixed outlier label and bins_method bug, added max.overlaps for outlier labels

--- R/histogram_sp.R
+++ R/histogram_sp.R
@@ -6,6 +6,7 @@
 #' @param bins_method Method for determining histogram bin width. Choices are "sturges", "scott", "fd" (Freedman-Diaconis) or a numeric value specifying the number of bins. Default is "sturges".
 #' @param facet_cols Variable to split data by in rows for faceted plots.
 #' @param facet_rows Variable to split data by in columns for faceted plots.
+#' @param max.overlaps Maximum number of overlaps allowed for outlier labels before suppression. Default is 20. Use "Inf" to disable suppression entirely.
 #'
 #' @returns ggplot2 histogram object with outliers labeled.
 #' @export
@@ -20,36 +21,81 @@
 #' facet_rows = INO
 #' )
 #'
-histogram_sp <- function(data, var, id = NULL, bins_method = "sturges", facet_cols = NULL, facet_rows = NULL) {
-  data %>%
+histogram_sp <- function(data, var, id = NULL, bins_method = "sturges", facet_cols = NULL, facet_rows = NULL, max.overlaps = 20) {
+  gobj <- data %>%
     ggplot2::ggplot(ggplot2::aes(x = {{var}})) +
-    ggplot2::geom_histogram(color=1, fill="white", binwidth = ~bins_method(.x, method))+
-    ggplot2::geom_density(ggplot2::aes(y = ggplot2::after_stat(count)), color = "red")+
-    ggplot2::geom_vline(ggplot2::aes(x = median({{var}}, na.rm = TRUE), y = {{var}}, xintercept = ggplot2::after_stat(y), color = "mean"), stat = "summary", fun = ~mean(.x), linetype = "dashed", na.rm = TRUE)+
-    ggplot2::geom_vline(ggplot2::aes(x = median({{var}}, na.rm = TRUE), y = {{var}}, xintercept = ggplot2::after_stat(y), color = "median"), stat = "summary", fun = ~median(.x), linetype = "dashed", na.rm = TRUE)+
-    ggplot2::geom_vline(ggplot2::aes(x = median({{var}}, na.rm = TRUE), y = {{var}}, xintercept = ggplot2::after_stat(y), color = "plus2sd"), stat = "summary", fun = ~mean(.x) + 2*sd(.x), linetype = "dashed", na.rm = TRUE)+
-    ggplot2::geom_vline(ggplot2::aes(x = median({{var}}, na.rm = TRUE), y = {{var}}, xintercept = ggplot2::after_stat(y), color = "min2sd"), stat = "summary", fun = ~mean(.x) - 2*sd(.x), linetype = "dashed", na.rm = TRUE)+
-    ggplot2::scale_color_manual(name = "statistics", values = c(
-      median = "green",
-      mean = "red",
-      min2sd = "blue",
-      plus2sd = "blue"
-    ),
-    labels = c(median = "Median", mean = "Mean", min2sd = "-2 SD", plus2sd = "+2 SD"))+
-    ggplot2::facet_grid(rows = dplyr::vars({{facet_rows}}), cols = dplyr::vars({{facet_cols}})) +
-    ggplot2::labs(x = dplyr::quo({{var}}))  -> gobj
+    #Histogram
+    ggplot2::geom_histogram(color = 1, fill = "white", binwidth = ~ bins_method(.x, bins_method)) +
+    #Density plot
+    ggplot2::geom_density(ggplot2::aes(y = ggplot2::after_stat(count)), color = "red") +
+    # Mean line
+    ggplot2::geom_vline(
+      ggplot2::aes(x = median({{var}}, na.rm = TRUE),  y = {{var}}, xintercept = ggplot2::after_stat(y), color = "mean"),
+      stat = "summary",
+      fun = ~ mean(.x),
+      linetype = "dashed",
+      na.rm = TRUE
+    ) +
+    # Median lineM
+    ggplot2::geom_vline(
+      ggplot2::aes(x = median({{var }}, na.rm = TRUE), y = {{var}}, xintercept = ggplot2::after_stat(y), color = "median"),
+      stat = "summary",
+      fun = ~ median(.x),
+      linetype = "dashed",
+      na.rm = TRUE
+    ) +
+    # +2 SD line
+    ggplot2::geom_vline(
+      ggplot2::aes(x = median({{var}}, na.rm = TRUE), y = {{var}}, xintercept = ggplot2::after_stat(y), color = "plus2sd"),
+      stat = "summary",
+      fun = ~ mean(.x) + 2 * sd(.x),
+      linetype = "dashed",
+      na.rm = TRUE
+    ) +
+    # -2 SD line
+    ggplot2::geom_vline(
+      ggplot2::aes(x = median({{var}}, na.rm = TRUE), y = {{var}}, xintercept = ggplot2::after_stat(y), color = "min2sd"),
+      stat = "summary",
+      fun = ~ mean(.x) - 2 * sd(.x),
+      linetype = "dashed",
+      na.rm = TRUE
+    ) +
+    # Color scale
+    ggplot2::scale_color_manual(
+      name = "statistics",
+      values = c(median = "green", mean = "red", min2sd = "blue", plus2sd = "blue"),
+      labels = c(median = "Median", mean = "Mean", min2sd = "-2 SD", plus2sd = "+2 SD")
+    ) +
+    # Facets if specified by facet_rows and/or facet_cols
+    ggplot2::facet_grid(rows = dplyr::vars({{ facet_rows }}), cols = dplyr::vars({{ facet_cols }})) +
+    # Var name as x-axis label
+    ggplot2::labs(x = dplyr::quo({{var}}))
 
-  ymax <- ggplot2::ggplot_build(gobj)$layout$panel_params[[1]]$y.range[2]
-  boxplot_width <- ymax * 0.1
+  ymax <- ggplot2::ggplot_build(gobj)$layout$panel_params[[1]]$y.range[2] # get y-axis maximum
+  boxplot_width <- ymax * 0.1 # set boxplot width as 10% of y-axis maximum
 
-  gobj +
-    ggplot2::geom_boxplot(position = ggplot2::position_nudge(y= -boxplot_width), width = boxplot_width, varwidth = TRUE)+
+  # Add boxplot and outlier labels
+  gobj + ggplot2::geom_boxplot(
+    position = ggplot2::position_nudge(y = -boxplot_width),
+    width = boxplot_width,
+    varwidth = TRUE
+  ) +
     ggrepel::geom_text_repel(
-      data = data %>% dplyr::group_by({{facet_rows}}, {{facet_cols}}) %>% dplyr::filter(is_outlier({{var}})),
-      y= -boxplot_width,
-      size = 3,
-      ggplot2::aes(label = ifelse(is.null({{id}}), rownames(data), {{id}}))
+      data = data %>% dplyr::group_by({{ facet_rows }}, {{ facet_cols }}) %>%
+        dplyr::mutate(
+          # Create id labels for outliers
+          id = dplyr::case_when(
+            is_outlier({{var}}) == FALSE ~ "", # not an outlier, no label
+            is.null({{id}}) ~ as.character(dplyr::row_number()), # If outlier and no id variable provided, use row number
+            .default = {{id}} # if outlier, use provided id variable
+          )
+        ) %>% dplyr::ungroup(),
+      y = -boxplot_width,
+      size = 2,
+      ggplot2::aes(label = id),
+      max.overlaps = max.overlaps
     )
 }
+
 
 utils::globalVariables(c("count", "y"))
