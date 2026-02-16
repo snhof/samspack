206d26ec39d0542fdc900ac9af58ebc5916b0af6
snhof <s.n.hof@amsterdamumc.nl>
2026-02-16 15:44:03 +0100

    Replaced ggpubr by patchwork to arrange plot

--- R/lmer_assump_tests.R
+++ R/lmer_assump_tests.R
@@ -21,58 +21,58 @@ lmer_assump_tests <- function(model) {
       )
     )
 
-  ranef_resid_plot <- try(performance::check_normality(model, effects = "random") %>% plot())
+  #Histogram for checking normality residuals
+  resid_histogram <- histogram_sp(data = data, var = .resid) +
+    ggplot2::labs(x="Residuals") +
+    ggplot2::theme(
+      legend.position="bottom",
+      legend.text = ggplot2::element_text(size=8),
+      legend.title = ggplot2::element_text(size=8),
+      legend.box.spacing = ggplot2::unit(0.1, "pt")
+    )
 
-  ggpubr::ggarrange(
-    #Normality residuals
-    histogram_sp(data = data, var = .resid) +
-      ggplot2::labs(x="Residuals") +
-      ggplot2::theme(
-        legend.position="bottom",
-        legend.text = ggplot2::element_text(size=8),
-        legend.title = ggplot2::element_text(size=8),
-        legend.box.spacing = ggplot2::unit(0.1, "pt")
-      ),
+  #Residuals vs fitted for checking linearity and homoscedasticity
+  resid_fitted_plot <- ggplot2::ggplot(data = data, ggplot2::aes(x = .fitted, y = .resid)) +
+    ggplot2::geom_point(alpha = 0.3) +
+    ggplot2::geom_smooth(method = 'loess', formula = "y ~ x", ggplot2::aes(color = "linear")) +
+    ggplot2::geom_smooth(method = 'lm', formula = "y ~ x", ggplot2::aes(color = "homosced", group = residual_type)) +
+    ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
+    ggplot2::scale_color_manual(
+      name = "Assumptions",
+      values = c(linear = "blue", homosced = "red"),
+      labels = c(linear = "Linearity: line flat and horizontal", homosced = "Homoscedasticity: lines parallel", min2sd = "-2 SD", plus2sd = "+2 SD")
+    )+
+    ggplot2::labs(x = "Fitted values", y = "Residuals") +
+    ggplot2::theme_minimal()+
+    ggplot2::theme(
+      axis.text = ggplot2::element_text(size=6),
+      axis.title = ggplot2::element_text(size=8),
+      legend.text = ggplot2::element_text(size=8),
+      legend.title = ggplot2::element_text(size=8),
+      legend.box.spacing = ggplot2::unit(0.1, "pt"),
+      legend.position="bottom"
+    )
 
-    ggpubr::ggarrange(
-      #Residuals vs fitted for checking linearity and homoscedasticity
-      ggplot2::ggplot(data = data, ggplot2::aes(x = .fitted, y = .resid)) +
-        ggplot2::geom_point(alpha = 0.3) +
-        ggplot2::geom_smooth(method = 'loess', formula = "y ~ x", ggplot2::aes(color = "linear")) +
-        ggplot2::geom_smooth(method = 'lm', formula = "y ~ x", ggplot2::aes(color = "homosced", group = residual_type)) +
-        ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
-        ggplot2::scale_color_manual(
-          name = "Assumptions",
-          values = c(linear = "blue", homosced = "red"),
-          labels = c(linear = "Linearity: line flat and horizontal", homosced = "Homoscedasticity: lines parallel", min2sd = "-2 SD", plus2sd = "+2 SD")
-        )+
-        ggplot2::labs(x = "Fitted values", y = "Residuals") +
-        ggplot2::theme_minimal()+
-        ggplot2::theme(
-          axis.text = ggplot2::element_text(size=6),
-          axis.title = ggplot2::element_text(size=8),
-          legend.text = ggplot2::element_text(size=8),
-          legend.title = ggplot2::element_text(size=8),
-          legend.box.spacing = ggplot2::unit(0.1, "pt"),
-          legend.position="bottom"
-        ) ,
+  # QQ-plot random effects
+  ranef_resid_plot_pre <- try(performance::check_normality(model, effects = "random") %>% plot())
 
-      # QQ-plot random effects
-      ranef_resid_plot[[1]]+
-        ggplot2::theme(
-          axis.text = ggplot2::element_text(size=6),
-          axis.title = ggplot2::element_text(size=8),
-          plot.title = ggplot2::element_text(size=9),
-          plot.subtitle = ggplot2::element_text(size=8)
-        ),
+  ranef_resid_plot<- ranef_resid_plot_pre[[1]]+
+    ggplot2::theme(
+      axis.text = ggplot2::element_text(size=6),
+      axis.title = ggplot2::element_text(size=8),
+      plot.title = ggplot2::element_text(size=9),
+      plot.subtitle = ggplot2::element_text(size=8)
+    )
 
-      nrow = 2
-    ),
 
-    ncol = 2
-  ) %>%
-    ggpubr::annotate_figure(top = ggpubr::text_grob(
-      formula(model) %>% format() %>% paste0(collapse = "") %>% stringr::str_squish(),
-      just = "left", x = 0, size = 9
-    ))
+  patchwork::wrap_plots(
+    resid_histogram,
+    resid_fitted_plot,
+    ranef_resid_plot,
+    design = "12
+              13"
+  ) +
+    patchwork::plot_annotation(
+    title = formula(model) %>% format() %>% paste0(collapse = "") %>% stringr::str_squish()
+  )
 }
