da3dbd93e6d0597b5b70b516a3c5d8acd3d869a2
snhof <s.n.hof@amsterdamumc.nl>
2026-05-21 10:33:07 +0200

    Fixed error when id is not named

--- R/check_histograms.R
+++ R/check_histograms.R
@@ -9,9 +9,9 @@
 #' @examples
 #' check_histograms(data = MS_trial_data, id = pat_id, facet_cols = gender)
 #'
-check_histograms <- function (data, facet_rows = NULL, facet_cols = NULL, id=NULL, bins_method = "sturges") {
+check_histograms <- function (data, facet_rows = NULL, facet_cols = NULL, id = dplyr::row_number(), bins_method = "sturges") {
 purrr::map(
-  .x = data %>% dplyr::select(dplyr::where(is.numeric), -{{id}}) %>% names(),
+  .x = data %>% dplyr::select(dplyr::where(is.numeric), -any_of(as.character(substitute({{id}})))) %>% names(),
   .f = ~histogram_sp(data=data, var = !!dplyr::sym(.x), id = {{id}}, bins_method = bins_method, facet_cols = {{facet_cols}}, facet_rows = {{facet_rows}})
 )
 }
